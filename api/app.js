766aae50b6c31df1ff5021a061ea0f6982cebe90
THISURA97 <tdinith481@gmail.com>
2021-11-05 21:45:31 +0530

    complete the basic functionality

--- api/app.js
+++ api/app.js
@@ -1,19 +1,34 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+var express = require('express');
+var morgan = require('morgan');
+var cors = require('cors');
 
-const app = express();
-const PORT = 5000;
+var cookieParser = require("cookie-parser");
+var createError = require("http-errors");
+var path = require("path");
+
+var indexRouter = require("./routes/index");
+var testAPIRouter = require("./routes/testAPI");
+var testDBRouter = require("./routes/testDB");
+
+var app = express();
+var PORT = 5000;
+
+
+app.set('view engine', 'jade');
 
 // setup middlewares
 
 app.use(morgan('dev'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use("/", indexRouter);
+app.use("/testAPI", testAPIRouter);
+app.use("/testDB", testDBRouter);
 
 app.get('/', (req, res) => {
   res.json('api is working!');
@@ -23,3 +38,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`API is listening on PORT: ${PORT}`);
 });
+
+
+
+module.exports = app;
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <p>docker-demo client</p>
-    </div>
-  );
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiResponse: "",
+            dbResponse: ""
+        };
+    }
+
+    // Go to API and check testAPI route for a response
+    callAPI() {
+        fetch("http://localhost:5000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+
+    // Go to API and check testDB route for a response
+    callDB() {
+        fetch("http://localhost:5000/testDB")
+            .then(res => res.text())
+            .then(res => this.setState({ dbResponse: res }))
+            .catch(err => err);
+    }
+
+    // Execute the calls when componnent mounts
+    componentDidMount() {
+        this.callAPI();
+        this.callDB();
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+                <p className="App-intro">{this.state.apiResponse}</p>
+                <p className="App-intro">{this.state.dbResponse}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
